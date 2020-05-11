b2c235e228ca56209ce8487968b80f6a204aea08
mc-santiago <mari.c.santiago@europe.com>
2020-05-11 22:39:04 +0200

    fixing css

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,4 @@
-console.log("Client side JavaScript file is loaded");
-
-const weatherForm = document.querySelector("form");
-const weatherImage = document.querySelector("#weatherImage");
-const description = document.querySelector("#description");
-const locationInput = document.querySelector("#locationInput");
-const temperature = document.querySelector("#temperature");
-const feelsLike = document.querySelector("#feelsLike");
-const precip = document.querySelector("#precip");
-const wind = document.querySelector("#wind");
-const lastUpdate = document.querySelector("#lastUpdate");
+console.log("Client side app.js JavaScript file is loaded");
 
 const toggleDarkMode = document.querySelector("#toggleDarkMode");
 let darkMode = localStorage.getItem("darkMode");
@@ -35,31 +25,4 @@ toggleDarkMode.addEventListener("click", () => {
   } else {
     disableDarkMode();
   }
-});
-
-weatherForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const location = locationInput.value;
-  locationInput.value = "Loading...";
-
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        console.log(data);
-        if (data.error) {
-          lastUpdate.textContent =
-            "Opps. It looks like there has been an error.";
-        } else {
-          locationInput.value = data.location;
-          weatherImage.src = data.forecast.weatherIcon;
-          description.textContent = data.forecast.description;
-          temperature.textContent = `${data.forecast.temperature} °C`;
-          feelsLike.textContent = `Feels Like: ${data.forecast.feelsLike} °C`;
-          precip.textContent = `Precip: ${data.forecast.precip}`;
-          wind.textContent = `Wind Speed(Dir): ${data.forecast.wind} (${data.forecast.windDir})`;
-          lastUpdate.textContent = `Last Update: ${new Date().toLocaleTimeString()}`;
-        }
-      });
-    }
-  );
 });
